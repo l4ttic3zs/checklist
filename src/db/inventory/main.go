0392a533f6cc7796eac6e84fea2230f797e44e45
bmzsombi <bmzsombi@gmail.com>
2025-12-24 15:16:33 +0100

    inventory

--- src/db/inventory/main.go
+++ src/db/inventory/main.go
@@ -1,5 +1,59 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"inventory/api"
+	"log"
+	"net/http"
+	"os"
 
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type App struct {
+	DB *gorm.DB
+}
+
+func main() {
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	port := "5432"
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		host, user, password, dbname, port)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		log.Fatalf("Cannot connect to database: %v", err)
+	}
+
+	app := &App{DB: db}
+
+	log.Println("Starting server on port 80...")
+	http.HandleFunc("/items", app.GetItems)
+
+	if err := http.ListenAndServe(":80", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (a *App) GetItems(w http.ResponseWriter, r *http.Request) {
+	var items []api.Item
+	// A Preload("ItemType") feltölti az Item struct ItemType mezőjét a másik táblából
+	result := a.DB.Preload("ItemType").Find(&items)
+
+	if result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(items)
 }
